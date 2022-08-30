ad4d5b3cf822b313d31d38101865e697221a9bc2
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-08-30 17:33:03 +0200

    Add search bar component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,25 @@
-import { useSelector } from 'react-redux';
-import Tile from './Tile';
-import '../styles/Home.css';
+import { useSelector } from "react-redux";
+import Tile from "./Tile";
+import SearchBar from "./SearchBar";
+import "../styles/Home.css";
 
 export default function Home() {
   const continents = useSelector((state) => state.continents);
 
   return (
     <div className="home-container">
-      {continents.map((continent, index) => (
-        <Tile
-          name={continent.name}
-          index={index}
-          key={continent.id}
-          id={continent.id}
-          type="isContinent"
-        />
-      ))}
+      <SearchBar />
+      <div className="list-container">
+        {continents.map((continent, index) => (
+          <Tile
+            name={continent.name}
+            index={index}
+            key={continent.id}
+            id={continent.id}
+            type="isContinent"
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,7 @@
+export default function SearchBar() {
+  return (
+    <div className="search-bar">
+      <input className="input" type="text" placeholder="search for a city" />
+    </div>
+  )
+}
