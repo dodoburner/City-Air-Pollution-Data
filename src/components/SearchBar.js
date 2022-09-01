85fcd35291d76e2544acc046c3484d6c410048af
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-09-01 13:02:04 +0200

    Setup search bar to get city data on submit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,35 @@
+// import { useNavigate } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux/es/exports';
+import { getCityLocation } from '../redux/citiesSlice';
+
 export default function SearchBar() {
+  // const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [location, setLocation] = useState('');
+  const cities = useSelector((state) => state.cities);
+  const city = cities.find((el) => el.name === location);
+  console.log(city);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(getCityLocation({ name: location }));
+  };
+
+  useEffect(() => {
+    console.log(cities);
+  }, [cities]);
+
   return (
-    <div className="search-bar">
-      <input className="input" type="text" placeholder="search for a city" />
-    </div>
+    <form className="search-bar" onSubmit={handleSubmit}>
+      <input
+        className="input"
+        type="text"
+        placeholder="search for a city"
+        value={location}
+        onChange={(e) => { setLocation(e.target.value); }}
+      />
+    </form>
   );
 }
--- src/redux/citiesSlice.js
+++ src/redux/citiesSlice.js
@@ -17,9 +17,7 @@ export default createSlice({
   extraReducers: (builder) => {
     builder.addCase('cities/getCityData/fulfilled', (state, action) => {
       const countryName = action.payload.topName;
-      const country = state.find(
-        (el) => el.name === countryName,
-      );
+      const country = state.find((el) => el.name === countryName);
       const city = country.cities.find((el) => el.name === action.payload.name);
       city.info = action.payload.results;
     });
@@ -34,41 +32,80 @@ export default createSlice({
       const country = action.payload.name;
       state.push({ name: country, cities });
     });
+
+    builder.addCase('cities/getCityLocation/fulfilled', (state, action) => {
+      state.push(action.payload);
+    });
   },
 });
 
-export const getCityData = createAsyncThunk('cities/getCityData', async ({
-  lat, long, topName, name,
-}) => {
-  const data = await axios.get(
-    `http://api.airvisual.com/v2/nearest_city?lat=${lat}&lon=${long}&key=d4281486-c6e5-40f2-a45a-666c2a800bae`,
-  );
-  const results = data.data.data.current;
-  return { results, topName, name };
-});
+export const getCityData = createAsyncThunk(
+  'cities/getCityData',
+  async ({
+    lat, long, topName, name,
+  }) => {
+    const data = await axios.get(
+      `http://api.airvisual.com/v2/nearest_city?lat=${lat}&lon=${long}&key=d4281486-c6e5-40f2-a45a-666c2a800bae`,
+    );
+    const results = data.data.data.current;
+    return { results, topName, name };
+  },
+);
 
-export const getCities = createAsyncThunk('cities/getCities', async ({ id, name }) => {
-  const where = encodeURIComponent(
-    JSON.stringify({
-      country: {
-        __type: 'Pointer',
-        className: 'Continentscountriescities_Country',
-        objectId: id,
+export const getCities = createAsyncThunk(
+  'cities/getCities',
+  async ({ id, name }) => {
+    const where = encodeURIComponent(
+      JSON.stringify({
+        country: {
+          __type: 'Pointer',
+          className: 'Continentscountriescities_Country',
+          objectId: id,
+        },
+      }),
+    );
+    const response = await fetch(
+      `https://parseapi.back4app.com/classes/Continentscountriescities_City?limit=20&order=-population&keys=name,location&where=${where}`,
+      {
+        headers: {
+          'X-Parse-Application-Id': 'mfpmjU4NFMM0RudR7jTsImrVvH16ZG7aqJhqWoiZ', // This is your app's application id
+          'X-Parse-REST-API-Key': '5kDXPpyzOXoLdqeid6koqIX7TVKpK97k1GEw33BK', // This is your app's REST API key
+        },
       },
-    }),
-  );
-  const response = await fetch(
-    `https://parseapi.back4app.com/classes/Continentscountriescities_City?limit=20&order=-population&keys=name,location&where=${where}`,
-    {
-      headers: {
-        'X-Parse-Application-Id':
-          'mfpmjU4NFMM0RudR7jTsImrVvH16ZG7aqJhqWoiZ', // This is your app's application id
-        'X-Parse-REST-API-Key':
-          '5kDXPpyzOXoLdqeid6koqIX7TVKpK97k1GEw33BK', // This is your app's REST API key
+    );
+    const data = await response.json();
+    const { results } = data; // Here you have the data that you need
+    return { results, name };
+  },
+);
+
+export const getCityLocation = createAsyncThunk(
+  'cities/getCityLocation',
+  async ({ name }) => {
+    const where = encodeURIComponent(
+      JSON.stringify({
+        name,
+      }),
+    );
+    const response = await fetch(
+      `https://parseapi.back4app.com/classes/Continentscountriescities_City?limit=1&keys=name,country,location&where=${where}`,
+      {
+        headers: {
+          'X-Parse-Application-Id': 'mfpmjU4NFMM0RudR7jTsImrVvH16ZG7aqJhqWoiZ', // This is your app's application id
+          'X-Parse-REST-API-Key': '5kDXPpyzOXoLdqeid6koqIX7TVKpK97k1GEw33BK', // This is your app's REST API key
+        },
       },
-    },
-  );
-  const data = await response.json();
-  const { results } = data; // Here you have the data that you need
-  return { results, name };
-});
+    );
+    const data = await response.json();
+    const { latitude, longitude } = data.results[0].location;
+    const { objectId } = data.results[0];
+
+    const cityAqi = await axios.get(
+      `http://api.airvisual.com/v2/nearest_city?lat=${latitude}&lon=${longitude}&key=d4281486-c6e5-40f2-a45a-666c2a800bae`,
+    );
+    const results = cityAqi.data.data.current;
+    const city = createCity(name, objectId, latitude, longitude);
+    city.info = results;
+    return city;
+  },
+);
